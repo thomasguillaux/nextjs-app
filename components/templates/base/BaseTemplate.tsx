f86942c05ee287ff64a40d5bcd5a001761b89dc7
thomasguillaux <thomasguillaux@gmail.com>
2022-06-26 21:42:19 +0200

    feat: implement search input

--- components/templates/base/BaseTemplate.tsx
+++ components/templates/base/BaseTemplate.tsx
@@ -1,9 +1,7 @@
-import styles from './BaseTemplate.module.css';
-
 export interface IBaseTemplate {}
 
 const BaseTemplate: React.FC<IBaseTemplate> = () => {
-  return <div className={styles.container}>Hello world!</div>;
+  return <div>Hello world!</div>;
 };
 
 export default BaseTemplate;
